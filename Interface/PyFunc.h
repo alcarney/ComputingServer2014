b35a83c2c7c23036b02b70b005593b3b31ed0c5a
ScrumFour <alex.rugby101@gmail.com>
2014-02-03 14:51:08 +0000

    Started fleshing out the PyFunc class made notes of data that will be coming from python

---
+++ Interface/PyFunc.h
@@ -0,0 +1,28 @@
+#ifndef PYFUNC_CPP
+#define PYFUNC_CPP
+
+#include <python2.7/Python.h>
+#include <iostream>
+
+// Values to get from python:
+//      int Quantity
+//      float Weight
+//      2 element array (floats) location
+//      3 elements array (unsigned int) Dims
+
+template <class Type>
+class PyFunc 
+{
+    private:
+        PyObject* pName, pArgs, pValue;
+
+    public:
+
+        PyFunc<Type>();
+        ~PyFunc<Type>();
+
+        void defineFunction(const char* Module, const char* FuncName);
+        Type callFunction(PyObject* pArgs);
+};
+
+#endif
