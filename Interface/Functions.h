af42114ddfe2e279ae32dd98e7d6a40eb02de85e
ScrumFour <alex.rugby101@gmail.com>
2014-02-13 14:50:33 +0000

    Absorbed the test.cpp code into Functions.h to illustrate usage for the new method

--- Interface/Functions.h
+++ Interface/Functions.h
@@ -63,3 +63,17 @@ Type* ReturnList<Type>::callFunction()
 }
 
 #endif
+
+/* Exmaple usage using the new method
+  
+#include "Functions.h"
+
+int main ()
+{
+    ReturnList<int> func("hello", "hello");
+
+    std::cout << *func.callFunction() << std::endl;
+
+    return 0;
+}
+ */
