7112cd97309068d5eee9ee78fd1e4e1ac8e58b36
ScrumFour <alex.rugby101@gmail.com>
2014-02-18 11:40:25 +0000

    Starting to work on a graph class

---
+++ tools/Graph.h
@@ -0,0 +1,23 @@
+// Declares everything needed to build a graph, most likely to be used with the salesman algorithm
+#ifndef GRAPH_CPP
+#define GRAPH_CPP
+
+#include <iostream>
+
+template <class Type>
+class Connection<Type>
+{
+    public:
+            Connection();
+
+}
+
+class Node
+{
+    public:
+            Node();
+            ~Node();
+
+}
+
+#endif
