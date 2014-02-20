8ffc57b784df7c70443015dc906800302f7d7545
ScrumFour <alex.rugby101@gmail.com>
2014-02-20 12:13:14 +0000

    Made a test re previous commit

--- tools/test.cpp
+++ tools/test.cpp
@@ -2,11 +2,41 @@
 
 class Dummy
 {
+    private:
+            double x;
+            double y;
     public: 
-        Dummy() {std::cout << "Hi there\n";};
+        Dummy(double longitude, double latitude);
+
+        void getInfo();
 };
+
+Dummy::Dummy(double longitude, double latitude)
+{
+    x = latitude;
+    y = longitude;
+}
+
+void Dummy::getInfo()
+{
+    std::cout << "x: " << x << " y: " << y << std::endl;
+}
 
 int main()
 {
-    Node<Dummy> dum;
+    LinkedList<Dummy>* dum = new LinkedList<Dummy>;
+
+    dum->appendNode(14.2,34.5);
+    dum->appendNode(90.4,-23.4);
+    dum->appendNode(256.7, 7654.4);
+
+    Dummy* node;
+
+    for (int i = 0; i < 3; i++)
+    {
+        node = dum->getNextNode();
+        node->getInfo();
+    }
+
+    return 0;
 }
