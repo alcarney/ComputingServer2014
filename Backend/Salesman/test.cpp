92002e7398701698f254de3d818ef57fe4295379
ScrumFour <alex.rugby101@gmail.com>
2014-02-25 12:18:28 +0000

    Test file changes, shows the situations where the aforementioned bug appears

--- Backend/Salesman/test.cpp
+++ Backend/Salesman/test.cpp
@@ -5,11 +5,11 @@ int main()
 {
     Salesman sales;
 
-    sales.addLocation(644.45, 65454.4);
-    sales.addLocation(343.54, 45456.43);
-    sales.addLocation(4322.44, 5454.32);
-    sales.addLocation (6554.6,  5454.4);
+    sales.addLocation (6, 4);
+    sales.addLocation (3, 4);
+
 
     sales.showLocations();
+    sales.populateMatrix();
 
 }
