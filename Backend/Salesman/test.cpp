70b3867c8314af0ec89dac7f4f7896a0889d94d0
ScrumFour <alex.rugby101@gmail.com>
2014-02-25 12:29:58 +0000

    New test

--- Backend/Salesman/test.cpp
+++ Backend/Salesman/test.cpp
@@ -7,6 +7,10 @@ int main()
 
     sales.addLocation (6, 4);
     sales.addLocation (3, 4);
+    sales.addLocation (-4, 4);
+    sales.addLocation (65, 4);
+    sales.addLocation (5.3, 7);
+    sales.addLocation (3, 7);
 
 
     sales.showLocations();
