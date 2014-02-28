f6cfcbce02aa727307fbeca2444bb3af7e628705
ScrumFour <alex.rugby101@gmail.com>
2014-02-28 15:08:12 +0000

    In the process of reworking the gui layout making it more flexible

--- GUI/updatedgui.py
+++ GUI/updatedgui.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import sys
 
 
@@ -20,6 +21,80 @@ except AttributeError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
 
+class Ui_FormEntry():
+
+        def __init__(self):
+            pass
+
+        def setup(self):
+
+            dataLayout = QtGui.QHBoxLayout()
+            # Each of These Contain the elements of the form, eg name,address etc
+            formElementName = QtGui.QVBoxLayout()
+            formElementAddress = QtGui.QVBoxLayout()
+            formElementPostcode = QtGui.QVBoxLayout()
+            formElementCoords = QtGui.QVBoxLayout()
+            formElementProd1 = QtGui.QVBoxLayout()
+            formElementProd2 = QtGui.QVBoxLayout()
+            formElementProd3 = QtGui.QVBoxLayout()
+            
+            """
+            gridLayout.addWidget(QtGui.QLabel("Product"),2,0)
+            gridLayout.addWidget(QtGui.QLabel("Quantity"),2,1)
+            
+            gridLayout.addWidget(QtGui.QLabel("Inspiron 15R"),3,0) # can't seem to get this above the middle of the page
+            gridLayout.addWidget(QtGui.QComboBox(),3,1)
+            gridLayout.addWidget(QtGui.QPushButton("Add new stock"),3,2)
+            """
+            formElementName.addWidget(QtGui.QLabel("Customer Name"))
+            formElementName.addWidget(QtGui.QLineEdit())
+            
+            formElementAddress.addWidget(QtGui.QLabel("First line of address"))
+            formElementAddress.addWidget(QtGui.QLineEdit())
+            
+            formElementPostcode.addWidget(QtGui.QLabel("Postcode"))
+            formElementPostcode.addWidget(QtGui.QLineEdit())
+            
+            formElementCoords.addWidget(QtGui.QLabel("Coordinates"))
+            formElementCoords.addWidget(QtGui.QLineEdit())
+            
+            formElementProd1.addWidget(QtGui.QLabel("Product 1"))
+            Product1 = QtGui.QComboBox()
+            formElementProd1.addWidget(Product1)
+            
+            formElementProd1.addWidget(QtGui.QLabel("Quantity"))
+            Prod1Quantity = QtGui.QComboBox()
+            formElementProd1.addWidget(Prod1Quantity)
+            
+            formElementProd2.addWidget(QtGui.QLabel("Product 2"))
+            Product2 = QtGui.QComboBox()
+            formElementProd2.addWidget(QtGui.QComboBox())
+            
+            formElementProd2.addWidget(QtGui.QLabel("Quantity"))
+            Prod2Quantity = QtGui.QComboBox()
+            formElementProd2.addWidget(Prod2Quantity)
+            
+            formElementProd3.addWidget(QtGui.QLabel("Product 3"))
+            Product3 = QtGui.QComboBox()
+            formElementProd3.addWidget(Product3)
+            
+            formElementProd3.addWidget(QtGui.QLabel("Quantity"))
+            Prod3Quantity = QtGui.QComboBox()
+            formElementProd3.addWidget(Prod3Quantity)
+
+            
+
+            dataLayout.addLayout(formElementName)
+            dataLayout.addLayout(formElementAddress)
+            dataLayout.addLayout(formElementPostcode)
+            dataLayout.addLayout(formElementCoords)
+            dataLayout.addLayout(formElementProd1)
+            dataLayout.addLayout(formElementProd2)
+            dataLayout.addLayout(formElementProd3)
+         
+
+            return dataLayout
+
 
 class Ui_MainWindow(QtGui.QMainWindow):
     # Attributes
@@ -28,19 +103,20 @@ class Ui_MainWindow(QtGui.QMainWindow):
     def __init__(self):
         super(Ui_MainWindow,self).__init__()
         self.setupUI()
-    
-    def setupUI(self):
-        
-                
-        '''
-        Arranges how big and where the interface on the screen will be
-        '''
 
+    def setupUI(self):
+
+        centralWidget = QtGui.QWidget()
+        bottomDock = QtGui.QDockWidget()
+        bottomDockWidget = QtGui.QWidget()
+
+
+        #Arranges how big and where the interface on the screen will be
         
+
+
         self.setWindowTitle('Axiom Enterprises - Route Logistics')
         self.setGeometry(200,200,750,750)  # window size and location
-        
-    
 
         exitAction = QtGui.QAction(QtGui.QIcon('exit.png'), '&Exit', self) # exit process     
         exitAction.setShortcut('Ctrl+Q')
@@ -54,107 +130,55 @@ class Ui_MainWindow(QtGui.QMainWindow):
        
         
         # These are used to create the overall look of the screen
-        mainLayout   = QtGui.QVBoxLayout()
+        self.mainLayout = QtGui.QVBoxLayout()
         titleLayout = QtGui.QHBoxLayout()
-        dataLayout = QtGui.QHBoxLayout()
+        contentLayout = QtGui.QHBoxLayout()
         buttonLayout = QtGui.QHBoxLayout()
 
-        # Each of These Contain the elements of the form, eg name,address etc
-        formElementName = QtGui.QVBoxLayout()
-        formElementAddress = QtGui.QVBoxLayout()
-        formElementPostcode = QtGui.QVBoxLayout()
-        formElementCoords = QtGui.QVBoxLayout()
-        formElementProd1 = QtGui.QVBoxLayout()
-        formElementProd2 = QtGui.QVBoxLayout()
-        formElementProd3 = QtGui.QVBoxLayout()
-        
-        centralWidget =     QtGui.QWidget()
-        """
-        gridLayout.addWidget(QtGui.QLabel("Product"),2,0)
-        gridLayout.addWidget(QtGui.QLabel("Quantity"),2,1)
-        
-        gridLayout.addWidget(QtGui.QLabel("Inspiron 15R"),3,0) # can't seem to get this above the middle of the page
-        gridLayout.addWidget(QtGui.QComboBox(),3,1)
-        gridLayout.addWidget(QtGui.QPushButton("Add new stock"),3,2)
-        """
-        formElementName.addWidget(QtGui.QLabel("Customer Name"))
-        formElementName.addWidget(QtGui.QLineEdit())
-        
-        formElementAddress.addWidget(QtGui.QLabel("First line of address"))
-        formElementAddress.addWidget(QtGui.QLineEdit())
-        
-        formElementPostcode.addWidget(QtGui.QLabel("Postcode"))
-        formElementPostcode.addWidget(QtGui.QLineEdit())
-        
-        formElementCoords.addWidget(QtGui.QLabel("Coordinates"))
-        formElementCoords.addWidget(QtGui.QLineEdit())
-        
-        formElementProd1.addWidget(QtGui.QLabel("Product 1"))
-        Product1 = QtGui.QComboBox()
-        formElementProd1.addWidget(Product1)
-        
-        formElementProd1.addWidget(QtGui.QLabel("Quantity"))
-        Prod1Quantity = QtGui.QComboBox()
-        formElementProd1.addWidget(Prod1Quantity)
-        
-        formElementProd2.addWidget(QtGui.QLabel("Product 2"))
-        Product2 = QtGui.QComboBox()
-        formElementProd2.addWidget(QtGui.QComboBox())
-        
-        formElementProd2.addWidget(QtGui.QLabel("Quantity"))
-        Prod2Quantity = QtGui.QComboBox()
-        formElementProd2.addWidget(Prod2Quantity)
-        
-        formElementProd3.addWidget(QtGui.QLabel("Product 3"))
-        Product3 = QtGui.QComboBox()
-        formElementProd3.addWidget(Product3)
-        
-        formElementProd3.addWidget(QtGui.QLabel("Quantity"))
-        Prod3Quantity = QtGui.QComboBox()
-        formElementProd3.addWidget(Prod3Quantity)
 
-        
+        self.mainLayout.addLayout(titleLayout)
+        l = Ui_FormEntry()
+        contentLayout.addLayout(l.setup())
+        self.mainLayout.addLayout(contentLayout)
+        self.mainLayout.addStretch(0.5)
+        #self.mainLayout.addLayout(buttonLayout)
 
-        dataLayout.addLayout(formElementName)
-        dataLayout.addLayout(formElementAddress)
-        dataLayout.addLayout(formElementPostcode)
-        dataLayout.addLayout(formElementCoords)
-        dataLayout.addLayout(formElementProd1)
-        dataLayout.addLayout(formElementProd2)
-        dataLayout.addLayout(formElementProd3)
-        self.addbutton = QtGui.QPushButton('Add Order')
-        dataLayout.addWidget(self.addbutton)
-        
-        
+        # The buttons
+        buttonLayout.addWidget(QtGui.QPushButton('Add Order'))
         buttonLayout.addStretch(1)
         buttonLayout.addWidget(QtGui.QPushButton("Ok"))
         buttonLayout.addWidget(QtGui.QPushButton("cancel"))
 
-        mainLayout.addLayout(titleLayout)
-        mainLayout.addLayout(dataLayout)
-        mainLayout.addStretch(0.5)
-        mainLayout.addLayout(buttonLayout)
- 
+        bottomDockWidget.setLayout(buttonLayout)
+        bottomDock.setWidget(bottomDockWidget)
+        bottomDock.setFeatures(QDockWidget.NoDockWidgetFeatures)
+        self.addDockWidget(Qt.BottomDockWidgetArea, bottomDock)
+
 
         self.setCentralWidget(centralWidget)
-        centralWidget.setLayout(mainLayout)
+        centralWidget.setLayout(self.mainLayout)
 
-        self.resize(750,750)       
-      
-        self.create_connections()
+        self.resize(750,750)
+
+        #self.create_connections()
+
+    def addEntry(self):
+        contentLayout = self.mainLayout.takeAt(1)
         
+        l = Ui_FormEntry()
+        
+        contentLayout.insertLayout(-1, l.setup())
+        self.mainLayout.insertLayout(1, contentLayout)
+        self.mainLayout.update()
 
     def create_connections(self):
-        self.addbutton.clicked.connect(self.closeEvent) 
+        self.addbutton.clicked.connect(self.addEntry) 
 
     def append():
         pass
 
-    
-        
-        
     def closeEvent(self, event):
-        
+
         reply = QtGui.QMessageBox.question(self, '!!!WARNING!!!',
             "Are you sure to quit? All data will be lost", QtGui.QMessageBox.Yes | 
             QtGui.QMessageBox.No, QtGui.QMessageBox.No)
