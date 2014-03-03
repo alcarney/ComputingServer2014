215983132b1194c5eecf0456ae160cbb72a9a31a
ScrumFour <alex.rugby101@gmail.com>
2014-03-03 17:26:31 +0000

    Added a file

---
+++ GUI/UI.cpp
@@ -0,0 +1,12 @@
+// This file manages the UI
+
+#include "../tools/Python/Functions.h"
+
+
+// This wont be in the final version since there is only one main()
+// function per C++ program but it will be commented out later and used
+// as a guide on how to use the functions defined here
+int main()
+{
+
+}
