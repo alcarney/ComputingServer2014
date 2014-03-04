6b8b93c58d5ad07093511a2a4ba734c6607ecf4b
ScrumFour <alex.rugby101@gmail.com>
2014-03-04 12:34:28 +0000

    Making moves to integrate the UI with the main program flow

--- GUI/UI.cpp
+++ GUI/UI.cpp
@@ -4,9 +4,33 @@
 #include "../tools/Python/Functions.h"
 
 
-// This wont be in the final version since there is only one main()
-// function per C++ program but it will be commented out later and used
-// as a guide on how to use the functions defined here
+// This function runs the login screen written in python, gets
+// a yay or nay signal and passes it back to C++ who then either 
+// closes or carries on executing the app
+bool userLogin()
+{
+    // Load the python login file 
+    ReturnList<bool>* login = new ReturnList<bool>("login", "main");
+
+    // Execute it
+    bool* ret = login->callFunction();
+
+    if (ret == NULL)
+    {
+        std::cout << "Something went wrong function returned null\n";
+        return false;
+    }
+    else
+    {
+        // Yay or nay?
+        return *ret;
+    }
+
+}
+
+/* This wont be in the final version since there is only one main()
+ function per C++ program but it will be commented out later and used
+ as a guide on how to use the functions defined here
 int main()
 {
     ReturnList<int>* login = new ReturnList<int>("login", "main");
@@ -23,4 +47,4 @@ int main()
     }
 
     return 0;
-}
+} */
