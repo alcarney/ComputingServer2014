7f00bca82d466ee5d5d1ba8891a4e5482deebaf4
ScrumFour <alex.rugby101@gmail.com>
2014-03-12 11:38:55 +0000

    Made the python client transmit data only

--- tools/Sockets/python_client_test.py
+++ tools/Sockets/python_client_test.py
@@ -8,6 +8,6 @@ PORT = 50007
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST,PORT))
 s.sendall('Hello World!')
-data = s.recv(1024)
+#data = s.recv(1024)
 s.close()
-print 'Recieved', repr (data)
+#print 'Recieved', repr (data)
