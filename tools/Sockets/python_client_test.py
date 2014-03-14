9e09ec40fe1046260a51712e5d9ea758df936faf
ScrumFour <alex.rugby101@gmail.com>
2014-03-14 11:19:25 +0000

    Changed the python test to see how the server handles a stream of data... Not well most _packets_ are dropped

--- tools/Sockets/python_client_test.py
+++ tools/Sockets/python_client_test.py
@@ -8,7 +8,8 @@ PORT = 3490
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST,PORT))
-s.sendall(struct.pack('ddd',1,23.4,45.6))
+for i in range(0,10):
+    s.sendall(struct.pack('ddd',i,23.4,45.6))
 #data = s.recv(1024)
 s.close()
 #print 'Recieved', repr (data)
