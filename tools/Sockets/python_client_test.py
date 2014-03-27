85707e0d982415e0af21c61d38edc9a23318eb88
ScrumFour <alex.rugby101@gmail.com>
2014-03-27 10:19:44 +0000

    altered the data sent so that it works with what the server is expecting

--- tools/Sockets/python_client_test.py
+++ tools/Sockets/python_client_test.py
@@ -17,20 +17,7 @@ s.sendall(struct.pack('i', 1))
 data = s.recv(4)
 
 # Tell the server how many locations we will send
-s.sendall(struct.pack('i', 9))
-
-# Transmit data
-for i in range (0,10):
-
-    s.sendall(struct.pack('ddd', i+1, 2*i, 3*i))
-
-    # Wait for acknowledgement before sending next piece of data - I still don't like this though
-    data = s.recv(8)
-    #print 'Recieved', repr (data)
-
-
-# Send EOS packet
-s.sendall(struct.pack('ddd', 0, 0, 0))
+s.sendall(struct.pack('ss', "alex", "password"))
 
 # Close the socket
 s.close()
